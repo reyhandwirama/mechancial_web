dbbc2c81bbe961853a3498e2394219da6fec962b
reyhan dwi rama <reyhandwirama@gmail.com>
2023-08-07 08:42:16 +0700

    bismillah last commit

--- src/components/global.js
+++ src/components/global.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const url = "https://zany-puce-lemming-tux.cyclic.app";
+const url = "https://delightful-lamb-jodhpurs.cyclic.app";
 const userData = JSON.parse(localStorage.getItem("user"));
 const boolUser = !!localStorage.getItem('user');
 
